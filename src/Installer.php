456014ba5a8a4ac9054310b10c7a565242235c1c
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2018-10-05 11:59:01 +0300

    lint fixes

--- src/Installer.php
+++ src/Installer.php
@@ -3,7 +3,8 @@
 namespace OpenCensus\Trace\Exporter;
 
 class Installer {
-    public static function checkPhpExtDependency() {
+    public static function checkPhpExtDependency()
+    {
         if (!function_exists('bcadd') && !function_exists('gmp_mul')) {
             throw new \Exception('`opencensus-php-exporter-jaeger` requires one of the two extensions to be installed: `php-bcmath` or `php-gmp`');
         }
